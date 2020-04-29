5ba370eae927bfd48348502acace79a1e12f4199
ogeorgeon <olivier.georgeon@gmail.com>
2020-04-29 16:36:05 +0200

    test

--- move_and_pose.py
+++ move_and_pose.py
@@ -54,7 +54,7 @@ class turtlebot():
         vel_msg.angular.z =0
         self.velocity_publisher.publish(vel_msg)
 
-        rospy.spin()
+        #rospy.spin()
 
     def move(self):
         # Receiveing the user's input
@@ -76,24 +76,23 @@ class turtlebot():
         vel_msg.angular.y = 0
         vel_msg.angular.z = 0
 
-        while not rospy.is_shutdown():
+        #while not rospy.is_shutdown():
+        # Setting the current time for distance calculus
+        t0 = float(rospy.Time.now().to_sec())
+        current_distance = 0
 
-            # Setting the current time for distance calculus
-            t0 = float(rospy.Time.now().to_sec())
-            current_distance = 0
-
-            # Loop to move the turtle in an specified distance
-            while (current_distance < distance):
-                # Publish the velocity
-                self.velocity_publisher.publish(vel_msg)
-                # Takes actual time to velocity calculus
-                t1 = float(rospy.Time.now().to_sec())
-                # Calculates distancePoseStamped
-                current_distance = speed * (t1 - t0)
-            # After the loop, stops the robot
-            vel_msg.linear.x = 0
-            # Force the robot to stop
+        # Loop to move the turtle in an specified distance
+        while (current_distance < distance):
+            # Publish the velocity
             self.velocity_publisher.publish(vel_msg)
+            # Takes actual time to velocity calculus
+            t1 = float(rospy.Time.now().to_sec())
+            # Calculates distancePoseStamped
+            current_distance = speed * (t1 - t0)
+        # After the loop, stops the robot
+        vel_msg.linear.x = 0
+        # Force the robot to stop
+        self.velocity_publisher.publish(vel_msg)
 
     def rotate(self):
         # Receiveing the user's input
@@ -131,7 +130,7 @@ class turtlebot():
         # Forcing our robot to stop
         vel_msg.angular.z = 0
         self.velocity_publisher.publish(vel_msg)
-        rospy.spin()
+        # rospy.spin()
 
 
 if __name__ == '__main__':
