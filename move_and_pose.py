9489381e4049bee9571b3ccd2f0434db99812efa
ogeorgeon <olivier.georgeon@gmail.com>
2020-04-29 16:03:16 +0200

    test

--- move_and_pose.py
+++ move_and_pose.py
@@ -3,8 +3,7 @@ import rospy
 from geometry_msgs.msg  import Twist
 from turtlesim.msg import Pose
 from math import pow,atan2,sqrt
-from move import move
-from rotate import rotate
+
 
 class turtlebot():
 
@@ -58,10 +57,82 @@ class turtlebot():
         rospy.spin()
 
     def move(self):
-        move()
+        # Receiveing the user's input
+        print("Let's move your robot")
+        speed = input("Input your speed:")
+        distance = input("Type your distance:")
+        isForward = input("Foward?: ")
+        vel_msg = Twist()
+
+        # Checking if the movement is forward or backwards
+        if (isForward):
+            vel_msg.linear.x = abs(speed)
+        else:
+            vel_msg.linear.x = -abs(speed)
+        # Since we are moving just in x-axis
+        vel_msg.linear.y = 0
+        vel_msg.linear.z = 0
+        vel_msg.angular.x = 0
+        vel_msg.angular.y = 0
+        vel_msg.angular.z = 0
+
+        while not rospy.is_shutdown():
+
+            # Setting the current time for distance calculus
+            t0 = float(rospy.Time.now().to_sec())
+            current_distance = 0
+
+            # Loop to move the turtle in an specified distance
+            while (current_distance < distance):
+                # Publish the velocity
+                self.velocity_publisher.publish(vel_msg)
+                # Takes actual time to velocity calculus
+                t1 = float(rospy.Time.now().to_sec())
+                # Calculates distancePoseStamped
+                current_distance = speed * (t1 - t0)
+            # After the loop, stops the robot
+            vel_msg.linear.x = 0
+            # Force the robot to stop
+            self.velocity_publisher.publish(vel_msg)
 
     def rotate(self):
-        rotate()
+        # Receiveing the user's input
+        print("Let's rotate your robot")
+        speed = input("Input your speed (degrees/sec):")
+        angle = input("Type your distance (degrees):")
+        clockwise = input("Clowkise?: ")  # True or false
+        vel_msg = Twist()
+
+        # Converting from angles to radians
+        angular_speed = speed * 2 * PI / 360
+        relative_angle = angle * 2 * PI / 360
+
+        # We wont use linear components
+        vel_msg.linear.x = 0
+        vel_msg.linear.y = 0
+        vel_msg.linear.z = 0
+        vel_msg.angular.x = 0
+        vel_msg.angular.y = 0
+
+        # Checking if our movement is CW or CCW
+        if clockwise:
+            vel_msg.angular.z = -abs(angular_speed)
+        else:
+            vel_msg.angular.z = abs(angular_speed)
+        # Setting the current time for distance calculus
+        t0 = rospy.Time.now().to_sec()
+        current_angle = 0
+
+        while (current_angle < relative_angle):
+            self.velocity_publisher.publish(vel_msg)
+            t1 = rospy.Time.now().to_sec()
+            current_angle = angular_speed * (t1 - t0)
+
+        # Forcing our robot to stop
+        vel_msg.angular.z = 0
+        self.velocity_publisher.publish(vel_msg)
+        rospy.spin()
+
 
 if __name__ == '__main__':
     try:
